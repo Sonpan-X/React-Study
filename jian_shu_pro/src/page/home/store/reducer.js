a2fc678a32dc89d8d5e95e1c81e37e9dfd2ff1a0
sonpan <13423516411@sina.cn>
2019-07-17 10:29:31 +0800

    try

--- jian_shu_pro/src/page/home/store/reducer.js
+++ jian_shu_pro/src/page/home/store/reducer.js
@@ -2,18 +2,10 @@ import * as constants from './constants';
 import { fromJS } from 'immutable';
 
 const defaultState = fromJS({
-topic_item:[
-  {
-    id:1,
-    title:'热门社会',
-    imgUrl:'//upload.jianshu.io/collections/images/18532/android.graphics.Bitmap_1c17451.jpeg?imageMogr2/auto-orient/strip|imageView2/1/w/144/h/144/format/webp'
-  },
-  {
-    id:2,
-    title:'',
-    imgUrl:''
-  }
-]
+topic_item:[],
+article_list:[],
+recom_list:[],
+author_list:[]
 });
 
 export default (state = defaultState, action) => {
